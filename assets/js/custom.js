4c02ce9e71d548a62397b6678900d35fd603ccee
Rafael Agapito <rafaelagapito@MacBook-Pro-de-Rafael.local>
2018-11-19 16:53:16 -0200

    last update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,28 +1,74 @@
 // Material Select Initialization
-$(document).ready(function() {
+$(document).ready(function () {
     $('.mdb-select').materialSelect();
 });
 
-function menuToggle() {
-    
-    var element = document.getElementById("mySidenav");
-    element.classList.toggle("toggle-menu");
 
-    var element2 = document.getElementById("menu-button");
-    element2.classList.toggle("toggle-menu-btn");
 
-    var element3 = document.getElementById("lista-menu");
-    element3.classList.toggle("block");
 
-    document.getElementById("link-block").addEventListener("click", function(event){
-        event.preventDefault()
-    });
-
-    // Bloqueia a interação de links desativados 
-    document.getElementById("disable-link").addEventListener("click", function(event){
-        event.preventDefault()
-    });
-    
+function getResult() {
+    document.getElementById("form").style.display = "none";
+    document.getElementById("carousel").style.display = "none";
+    document.getElementById("result").style.display = "block";
 }
 
+
+
+var color = Chart.helpers.color;
+var config = {
+    type: 'radar',
+    data: {
+        labels: [
+            ['Eating', 'Dinner'],
+            ['Drinking', 'Water'], 'Sleeping', ['Designing', 'Graphics'], 'Coding', 'Cycling', 'Running'
+        ],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: color(window.chartColors.red).alpha(0.2).rgbString(),
+            borderColor: window.chartColors.red,
+            pointBackgroundColor: window.chartColors.red,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        }, {
+            label: 'My Second dataset',
+            backgroundColor: color(window.chartColors.blue).alpha(0.2).rgbString(),
+            borderColor: window.chartColors.blue,
+            pointBackgroundColor: window.chartColors.blue,
+            data: [
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor(),
+                randomScalingFactor()
+            ]
+        }]
+    },
+    options: {
+        legend: {
+            position: 'top',
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Radar Chart'
+        },
+        scale: {
+            ticks: {
+                beginAtZero: true
+            }
+        }
+    }
+};
+
+window.onload = function () {
+    window.myRadar = new Chart(document.getElementById('canvas'), config);
+};
 
